bdef3f6ddebb67a8c8420bc92f746630dac5bceb
Eric Witowski <edubwitowski@gmail.com>
2018-10-15 01:32:56 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+$("#side-right").click(function(){
+    
+    var currentSlide = $(".longLineOfImg").css("left");
+        if (parseInt(currentSlide) <- 1600){
+        $(".longLineOfImg").css({
+                    left: 405
+
+
+        });   
+        }
+
+        $(".longLineOfImg").animate({
+            left: '-=405'
+        },1000);
+    });
+
+
+        $("#side-left").click(function(){
+
+            var currentSlide = $(".longLineOfImg").css("left"); 
+            if( parseInt(currentSlide) >= 0){
+                $(".longLineOfImg").css({left: -2023}); 
+            }
+    
+            $(".longLineOfImg").animate( {left: '+=405'}, 1000);  
+    
+        });
+    
+    });
